18c8520a99b75f81b542fea7bca547139a0abd2f
Aditya Malik <adityamalik360@gmail.com>
2018-02-19 19:12:29 +0530

    Create tui.js

---
+++ tui.js
@@ -0,0 +1,29 @@
+
+$(document).ready(function(){    
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+         var mm;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === "code") {
+          mm=sParameterName[1];
+        }
+    }
+
+		iframe = document.getElementById('ymIframeId');
+		
+	  iframe.contentWindow.postMessage(JSON.stringify({event_code: 'ym-client-event', data: JSON.stringify({
+			event: {
+				code: "Firstly",
+				data:{
+					code:mm
+					},
+					
+			}
+		})}), 'https://chat.botplatform.io');
+    })
+  
